e1cc25b3cd2492a9a817478669a6f56dd43997fd
eitnier <eitnier@gmail.com>
2020-04-22 16:50:20 +0000

    revert back to before dogs 9ca1c009cbfc515b7855148e206ef284b0057733

--- app.js
+++ app.js
@@ -1,3 +1,1 @@
 console.log("This is my complex app");
-console.log("Another quote");
-;dfjasjdf;lakdf a;ldkmf a;lskdmf a;lskdmf a;lfmd a;lfm asfm
--- cats.js
+++ cats.js
@@ -1,3 +1,1 @@
 console.log("meow");
-console.log("Purr");
-;dfjasjdf;lakdf a;ldkmf a;lskdmf a;lskdmf a;lfmd a;lfm asfm
